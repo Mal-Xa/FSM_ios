f2f28531574b4ea8b8e1708ea3f0f75f9f998c25
Mal-Xa <joelvanlalmalsawma@gmail.com>
2026-05-29 20:55:57 +0530

    ios reject fix

---
+++ App/public/assets/index-D0H11Rd9.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DTm9A-0v.js","assets/index-CTdzVgQj.js","assets/index-6zt52fTX.css"])))=>i.map(i=>d[i]);
+import{r as i,_ as r}from"./index-CTdzVgQj.js";const _=i("Device",{web:()=>r(()=>import("./web-DTm9A-0v.js"),__vite__mapDeps([0,1,2])).then(e=>new e.DeviceWeb)});export{_ as Device};
--- App/public/assets/index-hHoalDJh.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CkG7PbRK.js","assets/index-DMUWfeab.js","assets/index-BjF5Vn3z.css"])))=>i.map(i=>d[i]);
-import{r as i,_ as r}from"./index-DMUWfeab.js";const _=i("Device",{web:()=>r(()=>import("./web-CkG7PbRK.js"),__vite__mapDeps([0,1,2])).then(e=>new e.DeviceWeb)});export{_ as Device};
